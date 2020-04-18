8800101958c3bdca07d27d2bdd052e2a31c4ad76
Andres Cravioto <andres.cravioto@linio.com>
2020-04-18 18:29:27 -0500

    add user model login signup without token usage

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router  = express.Router();
 const Category = require('../models/Category.js');
-
+const User = require('../models/User.js');
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+const jwt = require('jwt-simple');
+const secret = process.env.PASS;
 
 router.post('/category/create', (req, res, next) => {
     const newCategory = new Category(req.body);
@@ -11,5 +15,34 @@ router.post('/category/create', (req, res, next) => {
     })
     .catch(e=>console.log(e))
 });
+
+router.post('/signup', (req, res, next) => {
+    console.log(req.body);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    req.body.password = hash;
+    const newUser = new User(req.body);
+    newUser.save()
+    .then(test=>{
+        return res.json({message: 'Usuario creado.'});
+    })
+    .catch( error => {
+        return res.status(500).json(error);
+    })
+});
+
+router.post('/login', (req, res, next) => {
+    let user = User.findOne({email: req.body.email})
+        .then( user => {
+            if(user && bcrypt.compareSync(req.body.password, user.password)) {
+                return res.status(200).json({token: jwt.encode({userId: user.id}, secret)});
+            }
+
+            return res.status(404).json({message: 'Crendenciales Invalidas.'});
+        })
+        .catch( error => {
+            return res.status(500).json(error);
+        })
+});
+
 
 module.exports = router;
